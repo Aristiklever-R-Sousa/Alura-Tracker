28947001c13a923591a7ace857aa0cf348acddc4
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2022-08-28 18:20:00 -0300

    feat: Faznendo o timer iniciar ao apertar no play e parar no stop.

--- src/components/FormComp.vue
+++ src/components/FormComp.vue
@@ -18,15 +18,15 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong> 00:00:00 </strong>
+            <strong> {{ elapsedTime }} </strong>
           </section>
-          <button class="button">
+          <button class="button" @click="start">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="stop">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -43,5 +43,27 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FormComp",
+  data: () => {
+    return {
+      timeInSeconds: 0,
+      stopwatch: 0,
+    };
+  },
+  computed: {
+    elapsedTime(): string {
+      return new Date(this.timeInSeconds * 1000).toISOString().slice(11, 19);
+    },
+  },
+  methods: {
+    start() {
+      this.stopwatch = setInterval(() => {
+        this.timeInSeconds++;
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.stopwatch);
+      this.timeInSeconds = 0;
+    },
+  },
 });
 </script>
