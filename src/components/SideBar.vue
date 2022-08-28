516e3210d30b4f1d01b23e43a317404c7d433055
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2022-08-28 13:19:09 -0300

    feat: Criando a interface de gerenciamento de tarefas.

---
+++ src/components/SideBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/alura-tracker-logo.png" alt="Alura Tracker" />
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SideBar",
+});
+</script>
+
+<style>
+/* Todo o estilo aqui */
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+
+img {
+  width: 100%;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
